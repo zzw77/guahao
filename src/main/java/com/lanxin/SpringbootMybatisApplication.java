51bbdedf0871c4203a6b5556986e7ff66438446d
zzw <zz@126.com>
2021-03-04 08:53:03 +0800

    zzw

--- src/main/java/com/lanxin/SpringbootMybatisApplication.java
+++ src/main/java/com/lanxin/SpringbootMybatisApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootMybatisApplication {//springboot启动类
 
     public static void main(String[] args) {
-        System.out.println("你们");
+        System.out.println("你们好");
         SpringApplication.run(SpringbootMybatisApplication.class, args);
     }
 
